ce13e67979652c54e8d4d850895f4b49dbc85b73
AbdurehmanSaleemi <asaleemi121@gmail.com>
2021-05-10 21:17:28 +0500

    init

--- Q1.cpp
+++ Q1.cpp
@@ -10,8 +10,8 @@ class myPair{
 
     //Default Constructor
     myPair(){
-        key = NULL;
-        value = NULL;
+        key = 0;
+        value = 0;
     }
 
     //Overloaded Constructor
@@ -29,7 +29,62 @@ class myPair{
     //Member Functions
 
     //Assignment Operator
-    myPair operator =(myPair pr){
-        
+    void operator =(myPair& pr){
+        pr.key = key;
+        pr.value = value;
+        //return *this;
     }
+
+    void swap(myPair &pr){
+        key = pr.key;
+        value = pr.value;
+    }
+
+    void operator ==(myPair &pr){
+        bool isEqual = false;
+        if(pr.key == key)
+        {
+            if(pr.value == value)
+            {
+                isEqual = true;
+            }
+        }
+        else if (pr.value == value)
+        {
+            if(pr.key == key)
+            {
+                isEqual = true;
+                
+            }
+        }
+        else
+        {
+            isEqual = false;
+        }
+        
+        if(isEqual)
+        {
+            std::cout << "Equal\n";
+        }
+        else
+        {
+            std::cout << "Not Equal\n";
+        }
+    }
+    void print(){
+        std::cout << key << std::endl;
+        std::cout << value << std:: endl;
+    } 
 };
+
+int main(){
+    myPair <int, float> P(2, 5.5);
+    myPair <int, float> P1(2,5.5);
+    myPair <int, float> P3;
+    //P.print();
+    //P = P1;
+    //P1.print();
+    //P1.swap(P);
+    //P1.print();
+    P == P1;
+}
