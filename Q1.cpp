e64072792df2d0e91f437f18fe7ba756e2a51d23
AbdurehmanSaleemi <asaleemi121@gmail.com>
2021-05-10 16:39:19 +0500

    init

--- Q1.cpp
+++ Q1.cpp
@@ -1,1 +1,35 @@
 #include <iostream>
+
+template <typename T1, typename T2>
+class myPair{
+    private:
+    T1 key;
+    T2 value;
+
+    public:
+
+    //Default Constructor
+    myPair(){
+        key = NULL;
+        value = NULL;
+    }
+
+    //Overloaded Constructor
+    myPair(T1 a, T2 b){
+        key = a;
+        value = b;
+    }
+
+    //Copy Constructor
+    myPair(myPair &pr){
+        pr.key = key;
+        pr.value = value;
+    }
+
+    //Member Functions
+
+    //Assignment Operator
+    myPair operator =(myPair pr){
+        
+    }
+};
