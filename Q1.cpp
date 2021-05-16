67eddd09f117cbde74f140376cbc965cdf29b9c2
AbdurehmanSaleemi <asaleemi121@gmail.com>
2021-05-16 21:59:40 +0500

    init

--- Q1.cpp
+++ Q1.cpp
@@ -1,78 +1,78 @@
 #include <iostream>
 
 template <typename T1, typename T2>
-class myPair{
-    private:
+class myPair {
+private:
     T1 key;
     T2 value;
 
-    public:
+public:
     static int objCount;
 
     //Default Constructor
-    myPair(){
+    myPair() {
         key = 0;
         value = 0;
         objCount++;
     }
 
     //Overloaded Constructor
-    myPair(T1 a, T2 b){
+    myPair(T1 a, T2 b) {
         key = a;
         value = b;
         objCount++;
     }
 
     //Copy Constructor
-    myPair(myPair &pr){
+    myPair(myPair& pr) {
         pr.key = key;
         pr.value = value;
     }
 
     //Member Functions
-    void setKey(T1 k){
+    void setKey(T1 k) {
         key = k;
     }
 
-    void setValue(T2 v){
+    void setValue(T2 v) {
         value = v;
     }
 
     //Assignment Operator
-    void operator =(myPair& pr){
+    void operator =(myPair& pr) {
         pr.key = key;
         pr.value = value;
         //return *this;
     }
 
-    void swap(myPair &pr){
+    void swap(myPair& pr) {
         key = pr.key;
         value = pr.value;
     }
 
-    void operator ==(myPair &pr){
+    void operator ==(myPair& pr) {
         bool isEqual = false;
-        if(pr.key == key)
+        if (pr.key == key)
         {
-            if(pr.value == value)
+            if (pr.value == value)
             {
                 isEqual = true;
             }
         }
         else if (pr.value == value)
         {
-            if(pr.key == key)
+            if (pr.key == key)
             {
                 isEqual = true;
-                
+
             }
         }
         else
         {
             isEqual = false;
         }
-        
-        if(isEqual)
+
+        if (isEqual)
         {
             std::cout << "Equal\n";
         }
@@ -81,15 +81,15 @@ class myPair{
             std::cout << "Not Equal\n";
         }
     }
-    void print(){
+    void print() {
         std::cout << key << std::endl;
-        std::cout << value << std:: endl;
+        std::cout << value << std::endl;
     }
-    T2 getValue() const {
+    T2 getValue(){
         return value;
     }
-    
-    T1 getKey() const{
+
+    T1 getKey(){
         return key;
     }
 };
@@ -98,50 +98,51 @@ template<typename T1, typename T2>
 int myPair<T1, T2>::objCount = 0;
 
 template <typename T1, typename T2>
-class myMap{
+class myMap {
 
-    private:
+private:
     int size;
     const int MAX_SIZE = 1000;
-    myPair<T1, T2> *container; //Container is object of myPair Class
+    myPair<T1, T2>* container; //Container is object of myPair Class
 
     // object passed is _map
-    public:
-    myMap(){
+public:
+    myMap() {
         size = 0;
-        container = nullptr;
+        container = new myPair<T1,T2>[size];
     }
 
-    myMap(myMap &_map){
+    myMap(myMap& _map) {
         size = _map.size;
         container = _map.container;
     }
 
-    void setSize(){
-        size = myPair<T1,T2>::objCount;
+    void setSize() {
+        size = 1;
     }
 
-    void insert(myPair<T1,T2> &pr, int index){
+    void insert(myPair<T1, T2>& pr, int index) {
 
         bool keyCheck = true;
-        container = new myPair<T1,T2>[size];
-        increaseSize(size);
+        size++;
+        increaseSize(container, size);
 
-        for (int i = 0; i < getSize() || keyCheck == true; i++){
-            if(pr.getKey() == container[i].myPair<T1,T2>::getKey()){
+        for (int i = 0; i < getSize(); i++) {
+            if (pr.getKey() == container[i].myPair<T1,T2>::getKey()){
                 keyCheck = false;
+                break;
             }
         }
-        if(keyCheck == false){
-            container[index].myPair<T1,T2>::setKey(pr.getKey());
-            container[index].myPair<T1,T2>::setValue(pr.getValue());
+        if (keyCheck == true){
+            container[index].myPair<T1, T2>::setKey(pr.getKey());
+            container[index].myPair<T1, T2>::setValue(pr.getValue());
         }
-        else{
+        else {
             std::cout << "Key Already Exists" << std::endl;
         }
     }
 
-    void operator =(myMap<T1,T2> &_map){
+    void operator =(myMap<T1, T2>& _map) {
         size = _map.size;
         container = new myPair<T1, T2>[_map.size];
 
@@ -151,92 +152,119 @@ class myMap{
         }
     }
 
-    int isEmpty(myMap &_map){
-        if(_map.size == 0){
+    int isEmpty(myMap& _map) {
+        if (_map.size == 0) {
             return 1;
         }
-        else{
+        else {
             return -1;
         }
         return 0;
     }
 
-    int getSize(){
+    int getSize() {
         return size;
     }
 
-    T2 &operator [] (T1 k){
+    T2 operator [] (T1 k){
+
+        bool isFound = false;
         for (int i = 0; i < getSize(); i++)
         {
-            if(k == container[i])
+            if(k == container[i].myPair<T1, T2>::getKey()){
+                isFound = true;
+                std::cout << "Match Found" << std::endl;
+                return container[i].myPair<T1, T2>::getValue();
+                break;
+            }
+            else if(i == getSize() && isFound == false)
             {
-                return i;
+                size++;
+                increaseSize(container,size);
+                container[size].myPair<T1, T2>::setValue(0);
+                container[size].myPair<T1, T2>::setKey(k);
+                std::cout << "Match not Found. Inserted a new value in container" << std::endl;
+                return container[size].myPair<T1, T2>::getValue();
             }
         }
-        for (int i = 0; i < getSize(); i++){
-            if(i == getSize()){
-                if(k != container[i]){
-                    
-                }
-            }
-        }
-        return 0;
+        return container[0].myPair<T1, T2>::getValue();
     }
-
-    void increaseSize(int newSize){
-        newSize = size + 1;
-        myPair<T1,T2> *temp = new myPair<T1,T2>[newSize];
-        for (int i = 0; i < size; i++)
+    void increaseSize(myPair<T1,T2>*& arr, int oldSize) {
+        int n = oldSize;
+        myPair<T1,T2>* tempArr = new myPair<T1,T2>[n];
+        for (int i = 0; i < oldSize; i++)
         {
-            temp[i] = container[i];
+            tempArr[i].myPair<T1,T2>::setKey(container[i].myPair<T1, T2>::getKey());
+            tempArr[i].myPair<T1, T2>::setValue(container[i].myPair<T1, T2>::getValue());
         }
-        size = newSize;
-        delete [] container;
-        container = temp;
+        oldSize = n;
+        delete[] arr;
+        arr = tempArr;
     }
 
-    myPair<T1,T2> getContainer(int index){
+    myPair<T1, T2> getContainer(int index) {
         return container[index];
     }
 
-    void remove(T1 key){
-        //removing the key and value
-        for (int i = 0; i < size; i++){
-            if (container[i].getKey() == key)
+    void remove(T1 key) {
+        
+        int index; // storing the index of the key to be removed
+        for (int i = 0; i < getSize(); i++) {
+            if (container[i].myPair<T1, T2>::getKey() == key)
             {
-                container[i].setKey(0);
-                container[i].setValue(0);
+                index = i;
             }
         }
-    }
-
-    void print(int index){
-        std::cout << container[index].getValue() << " " << container[index].getKey() << std::endl;
-    }
-
-    ~myMap(){
-        if (container != nullptr)
+        int n = size - 1;
+        myPair<T1,T2> *tempArr = new myPair<T1, T2>[n];
+        for (int i = index; i < getSize(); i++)
         {
-            delete [] container;
+            myPair<T1, T2> temp(container[i]);
+            container[i].myPair<T1, T2>::setKey(container[i + 1].myPair<T1, T2>::getKey());
+            container[i + 1] = temp;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            tempArr[i].myPair<T1, T2>::setKey(container[i].myPair<T1, T2>::getKey());
+            tempArr[i].myPair<T1, T2>::setValue(container[i].myPair<T1, T2>::getValue());
+        }
+        size = n;
+        delete [] container;
+        container = tempArr;
+    }
+
+    void print(int index) {
+        std::cout << container[index].getKey() << " " << container[index].getValue() << std::endl;
+    }
+
+    ~myMap() {
+        if (container != nullptr){
+            delete[] container;
             container = nullptr;
         }
     }
 };
 
-int main(){
+int main() {
     myPair <int, float> P(1, 5.5);
-    myPair <int, float> P1(2,5.5);
-    myPair <int, float> P3;
+    myPair <int, float> P1(2, 4.5);
+    myPair <int, float> P2(3, 1.5);
     myMap <int, float> M1;
     myMap <int, float> M2;
     M1.setSize();
-    M1.insert(P1,0);
-    M1.insert(P,1);
+    M1.insert(P1, 1);
+    M1.insert(P, 2);
+    M1.insert(P2, 3);
     // std::cout << M1.getSize() << std::endl;
     // std::cout << M1.isEmpty(M1) << std::endl;
     M1.print(1);
-    M1.print(0);
-    
+    M1.print(2);
+    M1.print(3);
+    std::cout << M1[4] << std::endl;
+    M1.remove(1);
+    M1.print(1);
+    M1.print(3);
+    std::cout << M1.getSize() << std::endl;
     //M1.print();
     //P.print();
     //P = P1;
